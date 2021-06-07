1e53dc12bad14511703b454fa5dc96464f8da013
hermabe <32880010+hermabe@users.noreply.github.com>
2021-06-07 16:41:40 +0200

    Changes needed to use VO with simulation framework

--- RVO.py
+++ RVO.py
@@ -1,198 +1,155 @@
-from math import ceil, floor, sqrt
-import copy
-import numpy
+import math
+from dataclasses import dataclass
+from typing import List, Tuple
 
-from math import cos, sin, tan, atan2, asin
-
-from math import pi as PI
+import numpy as np
 
 
-
-def distance(pose1, pose2):
-    """ compute Euclidean distance for 2D """
-    return sqrt((pose1[0]-pose2[0])**2+(pose1[1]-pose2[1])**2)+0.001
-
-
-def RVO_update(X, V_des, V_current, ws_model):
-    """ compute best velocity given the desired velocity, current velocity and workspace model"""
-    ROB_RAD = ws_model['robot_radius']+0.1
-    V_opt = list(V_current)    
-    for i in range(len(X)):
-        vA = [V_current[i][0], V_current[i][1]]
-        pA = [X[i][0], X[i][1]]
-        RVO_BA_all = []
-        for j in range(len(X)):
-            if i!=j:
-                vB = [V_current[j][0], V_current[j][1]]
-                pB = [X[j][0], X[j][1]]
-                # use RVO
-                transl_vB_vA = [pA[0]+0.5*(vB[0]+vA[0]), pA[1]+0.5*(vB[1]+vA[1])]
-                # use VO
-                #transl_vB_vA = [pA[0]+vB[0], pA[1]+vB[1]]
-                dist_BA = distance(pA, pB)
-                theta_BA = atan2(pB[1]-pA[1], pB[0]-pA[0])
-                if 2*ROB_RAD > dist_BA:
-                    dist_BA = 2*ROB_RAD
-                theta_BAort = asin(2*ROB_RAD/dist_BA)
-                theta_ort_left = theta_BA+theta_BAort
-                bound_left = [cos(theta_ort_left), sin(theta_ort_left)]
-                theta_ort_right = theta_BA-theta_BAort
-                bound_right = [cos(theta_ort_right), sin(theta_ort_right)]
-                # use HRVO
-                # dist_dif = distance([0.5*(vB[0]-vA[0]),0.5*(vB[1]-vA[1])],[0,0])
-                # transl_vB_vA = [pA[0]+vB[0]+cos(theta_ort_left)*dist_dif, pA[1]+vB[1]+sin(theta_ort_left)*dist_dif]
-                RVO_BA = [transl_vB_vA, bound_left, bound_right, dist_BA, 2*ROB_RAD]
-                RVO_BA_all.append(RVO_BA)                
-        for hole in ws_model['circular_obstacles']:
-            # hole = [x, y, rad]
-            vB = [0, 0]
-            pB = hole[0:2]
-            transl_vB_vA = [pA[0]+vB[0], pA[1]+vB[1]]
-            dist_BA = distance(pA, pB)
-            theta_BA = atan2(pB[1]-pA[1], pB[0]-pA[0])
-            # over-approximation of square to circular
-            OVER_APPROX_C2S = 1.5
-            rad = hole[2]*OVER_APPROX_C2S
-            if (rad+ROB_RAD) > dist_BA:
-                dist_BA = rad+ROB_RAD
-            theta_BAort = asin((rad+ROB_RAD)/dist_BA)
-            theta_ort_left = theta_BA+theta_BAort
-            bound_left = [cos(theta_ort_left), sin(theta_ort_left)]
-            theta_ort_right = theta_BA-theta_BAort
-            bound_right = [cos(theta_ort_right), sin(theta_ort_right)]
-            RVO_BA = [transl_vB_vA, bound_left, bound_right, dist_BA, rad+ROB_RAD]
-            RVO_BA_all.append(RVO_BA)
-        vA_post = intersect(pA, V_des[i], RVO_BA_all)
-        V_opt[i] = vA_post[:]
-    return V_opt
+@dataclass
+class VelocityObstacle:
+    apex: np.array
+    bound_left: np.array
+    bound_right: np.array
+    distance_to: float
+    radius: float
 
 
-def intersect(pA, vA, RVO_BA_all):
-    # print '----------------------------------------'
-    # print 'Start intersection test'
-    norm_v = distance(vA, [0, 0])
-    suitable_V = []
-    unsuitable_V = []
-    for theta in numpy.arange(0, 2*PI, 0.1):
-        for rad in numpy.arange(0.02, norm_v+0.02, norm_v/5.0):
-            new_v = [rad*cos(theta), rad*sin(theta)]
-            suit = True
-            for RVO_BA in RVO_BA_all:
-                p_0 = RVO_BA[0]
-                left = RVO_BA[1]
-                right = RVO_BA[2]
-                dif = [new_v[0]+pA[0]-p_0[0], new_v[1]+pA[1]-p_0[1]]
-                theta_dif = atan2(dif[1], dif[0])
-                theta_right = atan2(right[1], right[0])
-                theta_left = atan2(left[1], left[0])
-                if in_between(theta_right, theta_dif, theta_left):
-                    suit = False
-                    break
-            if suit:
-                suitable_V.append(new_v)
-            else:
-                unsuitable_V.append(new_v)                
-    new_v = vA[:]
-    suit = True
-    for RVO_BA in RVO_BA_all:                
-        p_0 = RVO_BA[0]
-        left = RVO_BA[1]
-        right = RVO_BA[2]
-        dif = [new_v[0]+pA[0]-p_0[0], new_v[1]+pA[1]-p_0[1]]
-        theta_dif = atan2(dif[1], dif[0])
-        theta_right = atan2(right[1], right[0])
-        theta_left = atan2(left[1], left[0])
+def velocity_towards_goal(position: np.array, goal: np.array, max_velocity: float) -> np.array:
+    GOAL_MARGIN = 0.1
+    direction = goal - position
+    norm = np.linalg.norm(direction)
+    if norm < GOAL_MARGIN:
+        return np.zeros(2)
+    return direction * max_velocity/norm
+
+
+def create_velocity_obstacles(position: np.array, obstacles: list) -> List[VelocityObstacle]:
+    velocity_obstacles = []
+    for obstacle in obstacles:
+        radius = max(obstacle.width, obstacle.length)
+        obstacle_velocity = obstacle.direction * obstacle.speed
+        vo_apex = position + obstacle_velocity
+        obstacle_vector = obstacle.position - position
+        angle_to_obstacle = np.arctan2(obstacle_vector[1], obstacle_vector[0])
+
+        distance_to_obstacle = np.linalg.norm(obstacle_vector)
+        distance_to_obstacle = max(distance_to_obstacle, 2*radius)
+
+        theta_BAort = np.arcsin(2*radius/distance_to_obstacle)
+        theta_ort_left = angle_to_obstacle+theta_BAort
+        bound_left = np.array([np.cos(theta_ort_left), np.sin(theta_ort_left)])
+        theta_ort_right = angle_to_obstacle-theta_BAort
+        bound_right = np.array([np.cos(theta_ort_right), np.sin(theta_ort_right)])
+
+        velocity_obstacles.append(VelocityObstacle(vo_apex, bound_left,
+                                                   bound_right, distance_to_obstacle, 2*radius))
+    return velocity_obstacles
+
+
+def theta_dif_right_left(velocity_obstacle: VelocityObstacle, new_velocity: np.array, position: np.array, return_dif: bool = False) -> Tuple[float, float, float]:
+    diff = new_velocity + position - velocity_obstacle.apex
+    theta_dif = np.arctan2(diff[1], diff[0])
+    theta_right = np.arctan2(velocity_obstacle.bound_right[1], velocity_obstacle.bound_right[0])
+    theta_left = np.arctan2(velocity_obstacle.bound_left[1], velocity_obstacle.bound_left[0])
+    if return_dif:
+        return theta_dif, theta_right, theta_left, diff
+    return theta_dif, theta_right, theta_left
+
+
+def check_is_suitable(velocity_obstacles: List[VelocityObstacle], new_velocity: np.array, position: np.array) -> bool:
+    # If in any velocity obstacle, it is unsuitable
+    for velocity_obstacle in velocity_obstacles:
+        theta_dif, theta_right, theta_left = theta_dif_right_left(velocity_obstacle, new_velocity, position)
         if in_between(theta_right, theta_dif, theta_left):
-            suit = False
-            break
-    if suit:
-        suitable_V.append(new_v)
+            return False
+    return True
+
+
+def find_suitable_unsuitable_velocities(velocity_obstacles: List[VelocityObstacle], velocity: np.array, position: np.array) -> Tuple[List[np.array], List[np.array]]:
+    velocity_norm = np.linalg.norm(velocity)
+    suitable_velocities = []
+    unsuitable_velocities = []
+    for theta in np.arange(0, 2*math.pi, 0.1):
+        for rad in np.arange(0.02, velocity_norm+0.02, velocity_norm/5.0):
+            new_velocity = rad*np.array([np.cos(theta), np.sin(theta)])
+            if check_is_suitable(velocity_obstacles, new_velocity, position):
+                suitable_velocities.append(new_velocity)
+            else:
+                unsuitable_velocities.append(new_velocity)
+
+    return suitable_velocities, unsuitable_velocities
+
+
+def intersect(position: np.array, desired_velocity: np.array, velocity_obstacles: List[VelocityObstacle]) -> np.array:
+    suitable_velocities, unsuitable_velocities = find_suitable_unsuitable_velocities(
+        velocity_obstacles, desired_velocity, position)
+
+    # Add desired velocity as well
+    if check_is_suitable(velocity_obstacles, desired_velocity, position):
+        suitable_velocities.append(desired_velocity)
     else:
-        unsuitable_V.append(new_v)
-    #----------------------        
-    if suitable_V:
-        # print 'Suitable found'
-        vA_post = min(suitable_V, key = lambda v: distance(v, vA))
-        new_v = vA_post[:]
-        for RVO_BA in RVO_BA_all:
-            p_0 = RVO_BA[0]
-            left = RVO_BA[1]
-            right = RVO_BA[2]
-            dif = [new_v[0]+pA[0]-p_0[0], new_v[1]+pA[1]-p_0[1]]
-            theta_dif = atan2(dif[1], dif[0])
-            theta_right = atan2(right[1], right[0])
-            theta_left = atan2(left[1], left[0])
+        unsuitable_velocities.append(desired_velocity)
+
+    if suitable_velocities:
+        # Return the suitable velocity closest to the desired velocity
+        return min(suitable_velocities, key=lambda v: np.linalg.norm(v - desired_velocity))
     else:
-        # print 'Suitable not found'
-        tc_V = dict()
-        for unsuit_v in unsuitable_V:
-            tc_V[tuple(unsuit_v)] = 0
-            tc = []
-            for RVO_BA in RVO_BA_all:
-                p_0 = RVO_BA[0]
-                left = RVO_BA[1]
-                right = RVO_BA[2]
-                dist = RVO_BA[3]
-                rad = RVO_BA[4]
-                dif = [unsuit_v[0]+pA[0]-p_0[0], unsuit_v[1]+pA[1]-p_0[1]]
-                theta_dif = atan2(dif[1], dif[0])
-                theta_right = atan2(right[1], right[0])
-                theta_left = atan2(left[1], left[0])
-                if in_between(theta_right, theta_dif, theta_left):
-                    small_theta = abs(theta_dif-0.5*(theta_left+theta_right))
-                    if abs(dist*sin(small_theta)) >= rad:
-                        rad = abs(dist*sin(small_theta))
-                    big_theta = asin(abs(dist*sin(small_theta))/rad)
-                    dist_tg = abs(dist*cos(small_theta))-abs(rad*cos(big_theta))
-                    if dist_tg < 0:
-                        dist_tg = 0                    
-                    tc_v = dist_tg/distance(dif, [0,0])
-                    tc.append(tc_v)
-            tc_V[tuple(unsuit_v)] = min(tc)+0.001
-        WT = 0.2
-        vA_post = min(unsuitable_V, key = lambda v: ((WT/tc_V[tuple(v)])+distance(v, vA)))
-    return vA_post 
+        return intersect_unsuitable(unsuitable_velocities, velocity_obstacles, position, desired_velocity)
+
+
+def intersect_unsuitable(unsuitable_velocities: List[np.array], velocity_obstacles: List[VelocityObstacle], position: np.array, desired_velocity: np.array) -> np.array:
+    WT = 0.2
+    best_velocity = unsuitable_velocities[0]
+    best_value = float('inf')
+
+    for unsuitable_velocity in unsuitable_velocities:
+        tc = []
+        for velocity_obstacle in velocity_obstacles:
+            rad = velocity_obstacle.radius
+            theta_dif, theta_right, theta_left, dif = theta_dif_right_left(
+                velocity_obstacle, unsuitable_velocity, position, return_dif=True)
+            if in_between(theta_right, theta_dif, theta_left):
+                small_theta = abs(theta_dif-0.5*(theta_left+theta_right))  # Some angle to the left of the VO?
+                what = abs(velocity_obstacle.distance_to*np.sin(small_theta))
+                rad = max(rad, what)
+                big_theta = np.arcsin(what/rad)
+                dist_tg = abs(velocity_obstacle.distance_to*np.cos(small_theta))-abs(rad*np.cos(big_theta))
+                dist_tg = max(0, dist_tg)
+                tc_v = dist_tg/np.linalg.norm(dif)
+                tc.append(tc_v)
+
+        value = WT/(min(tc) + 0.001) + np.linalg.norm(unsuitable_velocity - desired_velocity)
+        if value < best_value:
+            best_velocity = unsuitable_velocity
+            best_value = value
+    return best_velocity
+
+
+def VO_update(position: np.array, desired_velocity: np.array, obstacles: list) -> np.array:
+    velocity_obstacles = create_velocity_obstacles(position, obstacles)
+    return intersect(position, desired_velocity, velocity_obstacles)
+
 
 def in_between(theta_right, theta_dif, theta_left):
-    if abs(theta_right - theta_left) <= PI:
+    if abs(theta_right - theta_left) <= math.pi:
         if theta_right <= theta_dif <= theta_left:
             return True
         else:
             return False
     else:
-        if (theta_left <0) and (theta_right >0):
-            theta_left += 2*PI
+        if (theta_left < 0) and (theta_right > 0):
+            theta_left += 2*math.pi
             if theta_dif < 0:
-                theta_dif += 2*PI
+                theta_dif += 2*math.pi
             if theta_right <= theta_dif <= theta_left:
                 return True
             else:
                 return False
-        if (theta_left >0) and (theta_right <0):
-            theta_right += 2*PI
+        if (theta_left > 0) and (theta_right < 0):
+            theta_right += 2*math.pi
             if theta_dif < 0:
-                theta_dif += 2*PI
+                theta_dif += 2*math.pi
             if theta_left <= theta_dif <= theta_right:
                 return True
             else:
                 return False
-
-def compute_V_des(X, goal, V_max):
-    V_des = []
-    for i in range(len(X)):
-        dif_x = [goal[i][k]-X[i][k] for k in range(2)]
-        norm = distance(dif_x, [0, 0])
-        norm_dif_x = [dif_x[k]*V_max[k]/norm for k in range(2)]
-        V_des.append(norm_dif_x[:])
-        if reach(X[i], goal[i], 0.1):
-            V_des[i][0] = 0
-            V_des[i][1] = 0
-    return V_des
-            
-def reach(p1, p2, bound=0.5):
-    if distance(p1,p2)< bound:
-        return True
-    else:
-        return False
-    
-    
